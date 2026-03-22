879bf5808583e1b3f07631b29042b1c294d9c7fe
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-03-22 07:36:48 +0000

    feat(frontend): add basic login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
+import FormCard from "./_components/FormCard";
+import InfoCarousel from "./_components/InfoCarousel";
+
 export default function LoginPage() {
     return (
-        <div>
-            Login page
+        <div className="flex min-h-screen">
+            <div className="hidden md:flex md:flex-1 items-center justify-center">
+                <InfoCarousel />
+            </div>
+            <div className="flex flex-1 items-center justify-center">
+                <FormCard />
+            </div>
         </div>
     );
 }
