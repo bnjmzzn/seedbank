63322fd46b527991617da9db383e36ef0cd7fb42
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-03-22 07:39:36 +0000

    fix(frontend): add missing toaster component and set default theme to darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -12,8 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", geist.variable, "dark")}>
       <body>
+        <Toaster />
         {children}
       </body>
     </html>
