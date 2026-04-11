07c4902d8cdd27bca62229810458b1a55f73aef5
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-04-11 13:28:57 +0800

    refactor(client): merge avatar util into utils.ts

--- src/lib/client/avatar.ts
+++
@@ -1,3 +0,0 @@
-export function getAvatarUrl(username: string | null): string {
-    return `https://api.dicebear.com/9.x/fun-emoji/svg?seed=seedbank_${username}`;
-}
--- src/lib/client/utils.ts
+++ src/lib/client/utils.ts
@@ -4,3 +4,7 @@ export function logout() {
     storage.clearAuth();
     window.location.href = "/login";
 }
+
+export function getAvatarUrl(username: string | null): string {
+    return `https://api.dicebear.com/9.x/fun-emoji/svg?seed=seedbank_${username}`;
+}
