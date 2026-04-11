8a572053a1bb5c3a060334023c2331a07a168cac
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-04-11 17:07:41 +0800

    feat(components): improve ProfileMenu code and styling

--- src/components/layout/ProfileMenu.tsx
+++ src/components/layout/ProfileMenu.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
+import { Menu, MenuItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
+import type { PopoverOrigin } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
-import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import { useRouter } from "next/navigation";
 import { logout } from "@/lib/client/utils";
 
@@ -16,18 +12,22 @@ interface ProfileMenuProps {
     open: boolean;
     onClose: () => void;
     username: string;
+    anchorOrigin?: PopoverOrigin;
+    transformOrigin?: PopoverOrigin;
 }
 
-export default function ProfileMenu({ anchorEl, open, onClose, username }: ProfileMenuProps) {
+export default function ProfileMenu({
+    anchorEl,
+    open,
+    onClose,
+    username,
+    anchorOrigin = { horizontal: "left", vertical: "top" },
+    transformOrigin = { horizontal: "left", vertical: "bottom" },
+}: ProfileMenuProps) {
     const router = useRouter();
 
     function handleProfile() {
         router.push(`/users/${username}`);
-        onClose();
-    }
-
-    function handleReferral() {
-        router.push("/referral");
         onClose();
     }
 
@@ -41,22 +41,37 @@ export default function ProfileMenu({ anchorEl, open, onClose, username }: Profi
             anchorEl={anchorEl}
             open={open}
             onClose={onClose}
-            transformOrigin={{ horizontal: "left", vertical: "bottom" }}
-            anchorOrigin={{ horizontal: "left", vertical: "top" }}
+            anchorOrigin={anchorOrigin}
+            transformOrigin={transformOrigin}
             elevation={0}
             slotProps={{
+                list: {
+                    sx: { p: 1 },
+                },
                 paper: {
-                    sx: { minWidth: 180 },
+                    sx: {
+                        minWidth: 180,
+                        borderRadius: 1,
+                        border: "1px solid",
+                        borderColor: "divider",
+                    },
                 },
             }}
         >
             <MenuItem onClick={handleProfile}>
-                <ListItemIcon><PersonIcon fontSize="small" /></ListItemIcon>
+                <ListItemIcon><PersonIcon/></ListItemIcon>
                 <ListItemText>View profile</ListItemText>
             </MenuItem>
             <Divider />
-            <MenuItem onClick={handleLogout} sx={{ color: "error.main" }}>
-                <ListItemIcon><LogoutIcon fontSize="small" sx={{ color: "error.main" }} /></ListItemIcon>
+            <MenuItem
+                onClick={handleLogout}
+                sx={{
+                    color: "error.main",
+                    "&:hover": {
+                        backgroundColor: "error.dark",
+                    }
+                }}>
+                <ListItemIcon><LogoutIcon sx={{ color: "error.main" }} /></ListItemIcon>
                 <ListItemText>Logout</ListItemText>
             </MenuItem>
         </Menu>
