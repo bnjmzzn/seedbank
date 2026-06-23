2fc8a4e503887edcea0568db87d922196257f3a7
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-06-23 12:15:09 +0800

    refactor(client): update dicebear avatar

--- src/lib/client/utils.ts
+++ src/lib/client/utils.ts
@@ -3,14 +3,15 @@ import { storage } from "./storage";
 import { HistoryRow } from "@/types/db";
 import { ActivityRadarPoint } from "@/components/shared/data/ActivityRadarChart";
 import { BalanceTrendPoint } from "@/components/shared/data/BalanceTrendChart";
+import { CURRENCY_TICKER } from "../config";
 
 export function logout() {
     storage.clearAuth();
     window.location.href = "/login";
 }
 
-export function getAvatarUrl(username: string | null): string {
-    return `https://api.dicebear.com/9.x/fun-emoji/svg?seed=seedbank_${username}`;
+export function getAvatarUrl(username: string): string {
+    return `https://api.dicebear.com/10.x/glyphs/svg?seed=${CURRENCY_TICKER}_${username}`;
 }
 
 export function filterHistory(rows: HistoryRow[], type?: string): HistoryRow[] {
