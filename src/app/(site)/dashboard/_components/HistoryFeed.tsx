f8a27befd5f0e6f27e3466a46720e0e1f8a9c3ac
Benjamin <148628055+bnjmzzn@users.noreply.github.com>
2026-06-25 15:59:38 +0800

    fix(dashboardPage): fix layout of HistoryList

--- src/app/(site)/dashboard/_components/HistoryFeed.tsx
+++ src/app/(site)/dashboard/_components/HistoryFeed.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default function HistoryFeed({ rows, isLoading }: Props) {
     return (
         <Paper elevation={0} sx={{ display: "flex", flex: 1, flexDirection: "column" }}>
-            <HistoryTable rows={rows} isLoading={isLoading} maxRowsPerPage={10} limit={5} />
+            <HistoryTable rows={rows} isLoading={isLoading} maxRowsPerPage={5} limit={5} />
         </Paper>
     );
 }
